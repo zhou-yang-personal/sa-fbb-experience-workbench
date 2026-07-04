275c578773e7dfdb2a5858e4183ff52f1b9aef47
zhou-yang-personal <zhou.yang.personal@gmail.com>
2026-07-03 22:19:25 -0500

    refactor: mount modular workbench app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { WorkbenchApp } from './features/workbench/WorkbenchApp';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <WorkbenchApp />
   </React.StrictMode>,
 );
