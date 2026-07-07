b41ec0e562c76146aa23837faece72602a7ca1b7
zhou-yang-personal <zhou.yang.personal@gmail.com>
2026-07-07 12:36:14 -0500

    feat: expose all analytics ADS materialization APIs 1.0.33

--- src/features/workbench/analyticsStructuredApi.ts
+++ src/features/workbench/analyticsStructuredApi.ts
@@ -10,18 +10,7 @@ export type StructuredAnalyticsQuery = {
 };
 
 function req(settings: MySqlSettings, importBatchId: string, analysisRunId: string, query: StructuredAnalyticsQuery = {}) {
-  return {
-    req: {
-      settings,
-      import_batch_id: importBatchId,
-      analysis_run_id: analysisRunId,
-      page: query.page,
-      page_size: query.pageSize,
-      keyword: query.keyword,
-      sort_by: query.sortBy,
-      min_value: query.minValue,
-    },
-  };
+  return { req: { settings, import_batch_id: importBatchId, analysis_run_id: analysisRunId, page: query.page, page_size: query.pageSize, keyword: query.keyword, sort_by: query.sortBy, min_value: query.minValue } };
 }
 
 function etlReq(settings: MySqlSettings, importBatchId: string, analysisRunId: string) {
@@ -29,18 +18,15 @@ function etlReq(settings: MySqlSettings, importBatchId: string, analysisRunId: s
 }
 
 export const analyticsStructuredApi = {
-  kpis: (settings: MySqlSettings, importBatchId: string, analysisRunId: string, query?: StructuredAnalyticsQuery) =>
-    invoke<MetricCard[]>('analytics_get_kpi_summary', req(settings, importBatchId, analysisRunId, query)),
-  appRank: (settings: MySqlSettings, importBatchId: string, analysisRunId: string, query?: StructuredAnalyticsQuery) =>
-    invoke<MetricCard[]>('analytics_get_app_rank', req(settings, importBatchId, analysisRunId, query)),
-  hourlyTrend: (settings: MySqlSettings, importBatchId: string, analysisRunId: string, query?: StructuredAnalyticsQuery) =>
-    invoke<MetricCard[]>('analytics_get_hourly_trend', req(settings, importBatchId, analysisRunId, query)),
-  networkHotspots: (settings: MySqlSettings, importBatchId: string, analysisRunId: string, query?: StructuredAnalyticsQuery) =>
-    invoke<MetricCard[]>('analytics_get_network_hotspots', req(settings, importBatchId, analysisRunId, query)),
-  userProfiles: (settings: MySqlSettings, importBatchId: string, analysisRunId: string, query?: StructuredAnalyticsQuery) =>
-    invoke<MetricCard[]>('analytics_get_user_profiles', req(settings, importBatchId, analysisRunId, query)),
-  leadEvidence: (settings: MySqlSettings, importBatchId: string, analysisRunId: string, query?: StructuredAnalyticsQuery) =>
-    invoke<MetricCard[]>('analytics_get_lead_evidence_page', req(settings, importBatchId, analysisRunId, query)),
-  materializeAppRank: (settings: MySqlSettings, importBatchId: string, analysisRunId: string) =>
-    invoke<CommandAck>('analytics_materialize_app_rank', etlReq(settings, importBatchId, analysisRunId)),
+  kpis: (settings: MySqlSettings, importBatchId: string, analysisRunId: string, query?: StructuredAnalyticsQuery) => invoke<MetricCard[]>('analytics_get_kpi_summary', req(settings, importBatchId, analysisRunId, query)),
+  appRank: (settings: MySqlSettings, importBatchId: string, analysisRunId: string, query?: StructuredAnalyticsQuery) => invoke<MetricCard[]>('analytics_get_app_rank', req(settings, importBatchId, analysisRunId, query)),
+  hourlyTrend: (settings: MySqlSettings, importBatchId: string, analysisRunId: string, query?: StructuredAnalyticsQuery) => invoke<MetricCard[]>('analytics_get_hourly_trend', req(settings, importBatchId, analysisRunId, query)),
+  networkHotspots: (settings: MySqlSettings, importBatchId: string, analysisRunId: string, query?: StructuredAnalyticsQuery) => invoke<MetricCard[]>('analytics_get_network_hotspots', req(settings, importBatchId, analysisRunId, query)),
+  userProfiles: (settings: MySqlSettings, importBatchId: string, analysisRunId: string, query?: StructuredAnalyticsQuery) => invoke<MetricCard[]>('analytics_get_user_profiles', req(settings, importBatchId, analysisRunId, query)),
+  leadEvidence: (settings: MySqlSettings, importBatchId: string, analysisRunId: string, query?: StructuredAnalyticsQuery) => invoke<MetricCard[]>('analytics_get_lead_evidence_page', req(settings, importBatchId, analysisRunId, query)),
+  materializeAppRank: (settings: MySqlSettings, importBatchId: string, analysisRunId: string) => invoke<CommandAck>('analytics_materialize_app_rank', etlReq(settings, importBatchId, analysisRunId)),
+  materializeHourly: (settings: MySqlSettings, importBatchId: string, analysisRunId: string) => invoke<CommandAck>('ads_hour', etlReq(settings, importBatchId, analysisRunId)),
+  materializeUser: (settings: MySqlSettings, importBatchId: string, analysisRunId: string) => invoke<CommandAck>('ads_user', etlReq(settings, importBatchId, analysisRunId)),
+  materializeLead: (settings: MySqlSettings, importBatchId: string, analysisRunId: string) => invoke<CommandAck>('ads_lead', etlReq(settings, importBatchId, analysisRunId)),
+  materializeNetwork: (settings: MySqlSettings, importBatchId: string, analysisRunId: string) => invoke<CommandAck>('ads_net', etlReq(settings, importBatchId, analysisRunId)),
 };
